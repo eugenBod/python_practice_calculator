ed064bad584cfdb765164d8fc06f78b5303c534e
eugenBod <eugenybod@gmail.com>
2025-03-18 16:52:54 +0300

    The simplest python calculator

---
+++ main.py
@@ -0,0 +1,30 @@
+print("Добро пожаловать в простейший калькулятор.")
+
+try:
+    first_number = float(input("Введите первое число: "))
+    second_number = float(input("Введите второе число: "))
+except ValueError:
+    result = "Ошибка ввода. Введите число!!!"
+else:
+    message = ("Какая операция вам нужна:\nСложение: +\nВычитание: -\n"
+               "Умножение: *\nДеление: /\nВаш выбор (введите знак нужной операции):")
+    operation = input(message)
+    if operation == "+":
+        print("Операция сложения (+)")
+        result = first_number + second_number
+    elif operation == "-":
+        print("Операция вычитания (-)")
+        result = first_number - second_number
+    elif operation == "*":
+        print("Операция умножения (*)")
+        result = first_number * second_number
+    elif operation == "/":
+        try:
+            print("Операция деления (/)")
+            result = first_number / second_number
+        except ZeroDivisionError:
+            result = "Деление на ноль невозможно!!!"
+    else:
+        result = "Операция, которую ввел пользователь, неизвестна."
+
+print(f"Ответ: {result}")
